0d423366ed198c0d8c740bb4c292e49197d113d6
HosokawaRisako <risakonbu95@gmail.com>
2019-10-16 12:43:06 +0900

    add program(Ruby)

---
+++ kadai3.rb
@@ -0,0 +1,16 @@
+#! /usr/bin/env ruby
+rika=[65,80,67,35,58,60,72,75,68,92,36,50,2,58.5,46,42,78,62,84,70] 
+eigo=[44,87,100,63,52,60,58,73,55,86,29,56,89,23,65,84,64,27,86,84] 
+
+ 
+
+sumR=rika[0]+rika[1]+rika[2]+rika[3]+rika[4]+rika[5]+rika[6]+rika[7]+rika[8]+rika[9]+rika[10]+rika[11]+rika[12]+rika[13]+rika[14]+rika[15]+rika[16]+rika[17]+rika[18]+rika[19] 
+
+sumE=eigo[0]+eigo[1]+eigo[2]+eigo[3]+eigo[4]+eigo[5]+eigo[6]+eigo[7]+eigo[8]+eigo[9]+eigo[10]+eigo[11]+eigo[12]+eigo[13]+eigo[14]+eigo[15]+eigo[16]+eigo[17]+eigo[18]+eigo[19] 
+
+puts sumR
+puts sumE 
+
+
+
+
